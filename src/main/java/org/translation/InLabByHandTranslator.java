1c867c0e73046785bda8b3a01fb6a6348414648c
Marina Tanaka <aslisweak@gmail.com>
2024-09-23 19:46:03 -0400

    add jp

--- src/main/java/org/translation/InLabByHandTranslator.java
+++ src/main/java/org/translation/InLabByHandTranslator.java
@@ -64,6 +64,9 @@ public class InLabByHandTranslator implements Translator {
         else if ("zh".equals(language)) {
             translatedName = "加拿大";
         }
+        else if ("jp".equals(language)) {
+            translatedName = "カナダ";
+        }
         return translatedName;
     }
 }
