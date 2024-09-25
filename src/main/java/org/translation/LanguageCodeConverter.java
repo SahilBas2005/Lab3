ed4cee242cf28ee6edefed5e94c5b2652329da51
PLATA <abhiketan1@gmail.com>
2024-09-24 21:06:19 -0400

    syo

--- src/main/java/org/translation/LanguageCodeConverter.java
+++ src/main/java/org/translation/LanguageCodeConverter.java
@@ -5,6 +5,7 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -13,12 +14,14 @@ import java.util.Map;
  */
 public class LanguageCodeConverter {
 
-    // TODO Task: pick appropriate instance variables to store the data necessary for this class
-
+    private Map codeToLang = new HashMap();
+    private Map langToCode = new HashMap();
+    private Integer languageCount = 0;
     /**
      * Default constructor which will load the language codes from "language-codes.txt"
      * in the resources folder.
      */
+
     public LanguageCodeConverter() {
         this("language-codes.txt");
     }
@@ -33,12 +36,30 @@ public class LanguageCodeConverter {
         try {
             List<String> lines = Files.readAllLines(Paths.get(getClass()
                     .getClassLoader().getResource(filename).toURI()));
+            Iterator<String> lineIterator = lines.iterator();
+            while (lineIterator.hasNext()) {
+                String line = lineIterator.next().trim();
 
-            // TODO Task: use lines to populate the instance variable
-            //           tip: you might find it convenient to create an iterator using lines.iterator()
+                if (line.isEmpty() || line.startsWith("ISO Language Names")) {
+                    continue;
+                }
 
-        // TODO Checkstyle: '}' on next line should be alone on a line.
-        } catch (IOException | URISyntaxException ex) {
+                String[] tokens = line.split("\t");
+                String language = tokens[0];
+                languageCount++;
+                String code = tokens[1];
+                String[] languageArray = language.split(",");
+
+                for (String lang : languageArray) {
+                    String langEdit = lang.trim();
+                    System.out.println(langEdit);
+                    System.out.println(code);
+                    langToCode.put(langEdit, code);
+                    codeToLang.put(code, langEdit);
+                }
+            }
+        }
+        catch (IOException | URISyntaxException ex) {
             throw new RuntimeException(ex);
         }
 
@@ -50,8 +71,7 @@ public class LanguageCodeConverter {
      * @return the name of the language corresponding to the code
      */
     public String fromLanguageCode(String code) {
-        // TODO Task: update this code to use your instance variable to return the correct value
-        return code;
+        return codeToLang.get(code).toString();
     }
 
     /**
@@ -60,8 +80,7 @@ public class LanguageCodeConverter {
      * @return the 2-letter code of the language
      */
     public String fromLanguage(String language) {
-        // TODO Task: update this code to use your instance variable to return the correct value
-        return language;
+        return langToCode.get(language).toString();
     }
 
     /**
@@ -69,7 +88,6 @@ public class LanguageCodeConverter {
      * @return how many languages are included in this code converter.
      */
     public int getNumLanguages() {
-        // TODO Task: update this code to use your instance variable to return the correct value
-        return 0;
+        return languageCount;
     }
 }
